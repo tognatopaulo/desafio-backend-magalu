fbead96e5dff8699aadf3a467fe9cfe514318881
uiutuuz_deere <tognatopaulogusttavo@johndeere.com>
2025-02-01 10:19:43 -0300

    Starting project

---
+++ src/main/java/tech/tognati/magalums/MagalumsApplication.java
@@ -0,0 +1,13 @@
+package tech.tognati.magalums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagalumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagalumsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/tognati/magalums/MagalumsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.tognati.magalums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagalumsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
